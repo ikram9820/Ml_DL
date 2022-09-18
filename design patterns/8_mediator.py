d785eee07c1962c85de827fe47a8776e6046e827
ikram9820 <ikram9820khan@gmail.com>
2022-09-18 10:21:20 +0500

    v2

---
+++ design patterns/8_mediator.py
@@ -0,0 +1,19 @@
+'''
+Allows an object to encapsulate the communicaton between other objects.
+'''
+
+from abc import ABC
+
+
+class Mediator(ABC):
+    pass 
+
+class Colleague(ABC):
+    pass 
+
+class ConcreteMediator(Mediator):
+    pass 
+
+class ConcreteColleague(Colleague):
+    pass
+
